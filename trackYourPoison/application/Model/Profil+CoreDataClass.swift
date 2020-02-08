7eaf332fc0de3b3660a49c5209694079bb1f1c4f
Ines Langbauer <inlang9454@gmail.com>
2020-02-08 21:47:59 +0100

    profil

---
+++ trackYourPoison/application/Model/Profil+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Profil+CoreDataClass.swift
+//  trackYourPoison
+//
+//  Created by Ines Langbauer on 08.02.20.
+//  Copyright © 2020 Ines&Yvonne. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Profil)
+public class Profil: NSManagedObject {
+
+}
